91e361e5f35650db991b98df9cebf9d95efd0aa8
prakharchoudhary <prakhar2397@gmail.com>
2017-08-05 12:03:26 +0530

    add published post manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager,
+					self).get_queryset().filter(status='published')
+
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
@@ -16,7 +21,8 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-
+	objects = models.Manager()	# The default manager
+	published = PublishedManager() # Our custom manager
 	class Meta:
 		ordering = ('-publish',)
 
