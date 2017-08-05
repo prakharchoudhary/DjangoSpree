48a9c1fab52c8fcbe35353c224236d3e68722bea
prakharchoudhary <prakhar2397@gmail.com>
2017-08-05 13:43:44 +0530

    use generic list view to make code concise

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,28 @@
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
 
 # Create your views here.
-def post_list(request):
-	object_list = Post.published.all()
-	paginator = Paginator(object_list, 3) # 3 posts in each page
-	page = request.GET.get('page')
-	try:
-		posts = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer deliver the first page
-		posts = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range deliver last page of results
-		posts = paginator.page(paginator.num_pages)
-	return render(request,'blog/post/list.html',{'page': page, 'posts': posts})
+# def post_list(request):
+# 	object_list = Post.published.all()
+# 	paginator = Paginator(object_list, 3) # 3 posts in each page
+# 	page = request.GET.get('page')
+# 	try:
+# 		posts = paginator.page(page)
+# 	except PageNotAnInteger:
+# 		# If page is not an integer deliver the first page
+# 		posts = paginator.page(1)
+# 	except EmptyPage:
+# 		# If page is out of range deliver last page of results
+# 		posts = paginator.page(paginator.num_pages)
+# 	return render(request,'blog/post/list.html',{'page': page, 'posts': posts})
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
