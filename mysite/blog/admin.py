b0a903680131e8199ec7036459cb0f25a525933f
prakharchoudhary <prakhar2397@gmail.com>
2017-08-05 13:03:37 +0530

    add get_absolute_url to get reverse link for detail view

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
 	list_display = ('title', 'slug', 'author', 'publish', 'status')
 	list_filter = ('status', 'created', 'publish', 'author')
 	search_fields = ('title', 'body')
-	prepopulated_fields = {'slug': ('title',)}
+	prepopulated_fields = {'slug': ('title',)}	# populates the slug fields as we the title.
 	raw_id_fields = ('author',)
 	date_hierarchy = 'publish'
 	ordering = ['status', 'publish']
