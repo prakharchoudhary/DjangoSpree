7a488e65dc2b97fb37b90ba1179743f42213c5e1
prakharchoudhary <prakhar2397@gmail.com>
2017-08-07 17:37:38 +0530

    add: show similar posts in post_detail based on similar tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -57,11 +58,16 @@ def post_detail(request, year, month, day, post):
 	else:
 		comment_form = CommentForm()
 
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
 	return render(request,
 				'blog/post/detail.html',
 				{'post': post,
 				 'comments': comments,
-				 'comment_form': comment_form
+				 'comment_form': comment_form,
+				 'similar_posts': similar_posts,
 				})
 
 def post_share(request, post_id):
