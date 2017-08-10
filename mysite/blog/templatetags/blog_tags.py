82e0b66d99e76a47381d19ea406b973baecf5bb0
prakharchoudhary <prakhar2397@gmail.com>
2017-08-10 23:37:21 +0530

    feature: render markdown input for posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,11 @@
 from django import template
 from django.db.models import Count
 
+# importing following dependencies ensures that we can use markdown
+# syntax in our html
+from django.utils.safestring import mark_safe
+import markdown
+
 register = template.Library()
 
 from ..models import Post
@@ -18,3 +23,7 @@ def show_latest_posts(count=5):
 def get_most_commented_posts(count=5):
 	return Post.published.annotate(total_comments=Count('comments'))\
 						 .order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+	return mark_safe(markdown.markdown(text))
