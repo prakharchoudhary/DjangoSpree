d0fb5df88d3c307744c9e9b4543e370eed751565
prakharchoudhary <prakhar2397@gmail.com>
2017-08-14 23:19:47 +0530

    add change password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import (login, logout, logout_then_login,
+										password_change, password_change_done)
+
 from . import views
 
 urlpatterns = [
@@ -10,6 +12,10 @@ urlpatterns = [
 	url(r'^login/$', login, name='login'),
 	url(r'^logout/$', logout, name='logout'),
 	url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+
+	# change password urls
+	url(r'^password-change/$', password_change, name='password_change'),
+	url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 
 	# user views
 	url(r'^$', views.dashboard, name='dashboard'),
