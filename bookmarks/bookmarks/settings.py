b1c0caa6b1e3b4f781fcd0f08ed119f5cd6b9252
prakharchoudhary <prakhar2397@gmail.com>
2017-08-14 22:10:29 +0530

    add login functionality

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -122,4 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+            os.path.join(BASE_DIR, 'static'),
+)
