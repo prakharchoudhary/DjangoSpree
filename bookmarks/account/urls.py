250acd55b9b7d38a41572fdce8a969fe4e76ac97
prakharchoudhary <prakhar2397@gmail.com>
2017-08-15 00:24:38 +0530

    add forgot password functionality: reset password by following a one time generated link

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.views import (login, logout, logout_then_login,
-										password_change, password_change_done)
+										password_change, password_change_done,
+										password_reset, password_reset_done,
+										password_reset_confirm, password_reset_complete)
 
 from . import views
 
@@ -16,6 +18,14 @@ urlpatterns = [
 	# change password urls
 	url(r'^password-change/$', password_change, name='password_change'),
 	url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+
+	# restore password urls
+	url(r'^password-reset/$', password_reset, name='password_reset'),
+	url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+	url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm,
+		name='password_reset_confirm'),
+	url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 
 	# user views
 	url(r'^$', views.dashboard, name='dashboard'),
