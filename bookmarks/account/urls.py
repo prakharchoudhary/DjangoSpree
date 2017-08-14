6279a1ea2ddd6b4d889c37580a03362e1c5cfc71
prakharchoudhary <prakhar2397@gmail.com>
2017-08-14 22:33:37 +0530

    add django authentication views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout, logout_then_login
 from . import views
 
 urlpatterns = [
 	# post views
-	url(r'^login/$', views.user_login, name='login'),
+	# url(r'^login/$', views.user_login, name='login'),
+
+	# login / logout urls
+	url(r'^login/$', login, name='login'),
+	url(r'^logout/$', logout, name='logout'),
+	url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
 ]
