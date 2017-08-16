1c227ee18c337edcad28ca2a71b9610609f546fb
prakharchoudhary <prakhar2397@gmail.com>
2017-08-17 00:22:55 +0530

    add status message for profile update

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Profile
 from .forms import (LoginForm, UserRegistrationForm,
@@ -76,6 +77,10 @@ def edit(request):
 		if user_form.is_valid() and profile_form.is_valid():
 			user_form.save()
 			profile_form.save()
+			messages.success(request, 'Profile updated successfully')
+
+		else:
+			messages.error(request, 'Error updating your profile')
 
 	else:
 		user_form = UserEditForm(instance=request.user)
