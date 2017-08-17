74a26a405decd90c72eab2e2ee67b4118e6526f0
prakharchoudhary <prakhar2397@gmail.com>
2017-08-17 23:22:26 +0530

    add custom authentication backend

---
+++ bookmarks/account/authentication.py
@@ -0,0 +1,28 @@
+"""
+Our custom authentication backend 
+"""
+
+from django.contrib.auth.models import User
+
+class EmailAuthBackend(object):
+	"""
+	Authenticate using e-mail account
+	"""
+	def authenticate(self, username=None, password=None):
+		
+		try:
+			user = User.objects.get(email=username)
+			if user.check_password(password):
+				return user
+			return None
+
+		except User.DoesNotExist:
+			return None
+
+	def get_user(self, user_id):
+
+		try:
+			return User.object.get(pk=user_id)
+		
+		except User.DoesNotExist:
+			return None	
