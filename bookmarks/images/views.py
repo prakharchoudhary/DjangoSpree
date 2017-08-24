0aad325d820a7cf2d95af7f6cd6f19ac49c5a684
prakharchoudhary <prakhar2397@gmail.com>
2017-08-25 01:59:57 +0530

    like functionality complete

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
 from django.core.urlresolvers import reverse
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 @login_required
@@ -35,5 +37,21 @@ def image_detail(request, id, slug):
 	image = get_object_or_404(Image, id=id, slug=slug)
 	return render(request,
 					'images/image/detail.html',
-					{'section': 'images',
-					'image': image})
+					{'section': 'images', 'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+	image_id = request.POST.get('id')
+	action = request.POST.get('action')
+	if image_id and action:
+		try:
+			image = Image.objects.get(id=image_id)
+			if action == 'like':
+				image.users_like.add(request.user)
+			else:
+				image.users_like.remove(request.user)
+			return JsonResponse({'status': 'ok'})
+		except:
+			pass
+	return JsonResponse({'status': 'ko'})
