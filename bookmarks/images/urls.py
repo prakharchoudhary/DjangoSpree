b7f423d32da9e8dcbdf8f360c584e4bd469a3166
prakharchoudhary <prakhar2397@gmail.com>
2017-08-24 23:52:20 +0530

    add views and routes for like functionality

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^create/$', views.image_create, name='create'),
-	url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail')
+	url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
+	url(r'^like/$', views.image_like, name='like'),
 ]
