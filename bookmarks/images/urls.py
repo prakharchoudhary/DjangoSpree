5941b90610ee391551c5c580e9591ce3113a02bf
prakharchoudhary <prakhar2397@gmail.com>
2017-08-27 23:53:23 +0530

    add pagination

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.image_list, name='list'),
 	url(r'^create/$', views.image_create, name='create'),
 	url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
 	url(r'^like/$', views.image_like, name='like'),
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -7,6 +7,8 @@ from .models import Image
 from django.core.urlresolvers import reverse
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from common.decorators import ajax_required
 # Create your views here.
 
@@ -57,3 +59,30 @@ def image_like(request):
 		except:
 			pass
 	return JsonResponse({'status': 'ko'})
+
+
+@login_required
+def image_list(request):
+	images = Image.objects.all()
+	paginator = Paginator(images, 8)
+	page = request.GET.get('page')
+	try:
+		images = paginator.page(page)
+	
+	except PageNotAnInteger:
+		if request.is_ajax():
+			# If page is not an integer deliver the first page
+			images = paginator.page(1)
+	
+	except EmptyPage:
+		if request.is_ajax():
+			# If the request AJAX and the page is out of range
+			# return an empty page
+			return HttpResponse('')
+		# If page is out of range deliver last page of results
+		images = paginator.page(paginator.num_pages)
+	
+	if request.is_ajax():
+		return render(request, 'images/image/list_ajax.html',{'section': 'images', 'images': images})
+	
+	return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
