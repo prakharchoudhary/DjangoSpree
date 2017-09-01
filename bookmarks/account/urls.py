a28047db35c6b0583684999e65a48177b5e36653
prakharchoudhary <prakhar2397@gmail.com>
2017-09-01 23:51:38 +0530

    add follow users ajax method

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
 	url(r'^edit/$', views.edit, name='edit'),
 	url(r'^users/$', views.user_list, name='user_list'),
 	url(r'^users/(?P<username>[-\w]+)/$', views.user_detail, name='user_detail'),
+	url(r'^users/follow/$', views.user_follow, name='user_follow'),
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 
-from .models import Profile
+from .models import Profile, Contact
 from .forms import (LoginForm, UserRegistrationForm,
 					UserEditForm, ProfileEditForm)
 
@@ -100,3 +102,23 @@ def user_detail(request, username):
 	user = get_object_or_404(User, username=username, is_active=True)
 	return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
 	
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+	user_id = request.POST.get('id')
+	action = request.POST.get('action')
+	if user_id and action:
+		try:
+			user = User.objects.get(id=user_id)
+			if action == 'follow':
+				Contact.objects.get_or_create(
+					user_from = request.user,
+					user_to=user)
+			else:
+				Contact.objects.filter(user_from=request.user, 
+					user_to=user).delete()
+			return JsonResponse({'status': 'ok'})
+		except User.DoesNotExist:
+			return JsonResponse({'status': 'ko'})
+	return JsonResponse({'status': 'ko'})
