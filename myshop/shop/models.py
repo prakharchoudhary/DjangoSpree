c873963488a4b774178458cdf50bb35d4161d1df
prakharchoudhary <prakhar2397@gmail.com>
2017-09-14 01:26:30 +0530

    add view and url for catalog

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -18,6 +18,10 @@ class Category(models.Model):
 	def __str__(self):
 		return self.name
 
+	def get_absolute_url(self):
+		return reverse('shop:product_list_by_category', args=[self.slug])
+
+
 class Product(models.Model):
 	category = models.ForeignKey(Category,
 								related_name='products')
@@ -37,4 +41,7 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return reverse('shop:product_detail', args=[self.id, self.slug])
 		
