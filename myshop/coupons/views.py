015d0f795df822c6a82dbd11187e8054ce1fe9bc
prakharchoudhary <prakhar2397@gmail.com>
2017-10-28 01:33:41 +0530

    applying discount on orders; functionality complete

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 from .models import Coupon
