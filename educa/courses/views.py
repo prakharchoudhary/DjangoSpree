5223a6267bc929041a393ab216d6177cf7750dbd
prakharchoudhary <prakhar2397@gmail.com>
2018-06-26 11:40:29 +0530

    add class-based views and templates to add, edit and delete courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from .models import Course
 
 # Create your views here.
+class OwnerMixin(object):
+
+	def get_queryset(self):
+		qs = super(OwnerMixin, self).get_queryset()
+		return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+
+	def form_valid(self, form):
+		form.instance.owner = self.request.user
+		return super(OwnerEditMixin, self).form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
+	
+	model = Course
+	fields = ['subject', 'title', 'slug', 'overview']
+	success_url = reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+	
+	fields = ['subject', 'title', 'slug', 'overview']
+	success_url = reverse_lazy('manage_course_list')
+	template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+
+	template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+	
+	permission_required = 'courses.add_course'
+
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+
+	template_name = 'courses/manage/course/form.html'
+	permission_required = 'courses.change_course'
+
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
+	
+	template_name = 'courses/manage/course/delete.html'
+	success_url = reverse_lazy('manage_course_list')
+	permission_required = 'courses.delete_course'
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
 	url(r'^accounts/login/$', auth_views.login, name='login'),
 	url(r'^accounts/logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^course/', include('courses.urls')),
 ]
