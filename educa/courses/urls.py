20c5a1d9eb5d00288ebe16d238525ba8cc5fad09
prakharchoudhary <prakhar2397@gmail.com>
2018-06-26 13:44:02 +0530

    reordering module and content via drag-n-drop

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -37,4 +37,12 @@ urlpatterns = [
 	url(r'^content/(?P<id>\d+)/delete/$',
 		views.ContentDeleteView.as_view(),
 		name='module_content_delete'),
+
+	url(r'^module/order/$',
+		views.ModuleOrderView.as_view(),
+		name='module_order'),
+
+	url(r'^content/order/$',
+		views.ContentOrderView.as_view(),
+		name='content_order'),
 ]
--- educa/courses/views.py
+++ educa/courses/views.py
@@ -6,6 +6,7 @@ from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
 from .models import Course, Module, Content
 
@@ -150,3 +151,20 @@ class ModuleContentListView(TemplateResponseMixin, View):
 			course__owner=request.user)
 
 		return self.render_to_response({'module': module})
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+
+	def post(self, request):
+		for id, order in self.request_json.items():
+			Module.objects.filter(id=id, course__owner=request.user)\
+			.update(order=order)
+
+		return self.render_json_response({'saved': 'OK'})
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+	
+	def post(self, request):
+		for id, order in self.request_json.items():
+			Content.objects.filter(id=id, 
+				module__course__owner=request.user).update(order=order)
+		return self.render_json_response({'saved': 'OK'})
